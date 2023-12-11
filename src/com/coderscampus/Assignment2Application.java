3a14ce179d9acc0b43496982ce27a268f83feba7
Mondo009 <dossantos989@gmail.com>
2023-12-11 16:11:23 -0500

    assignment2

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,10 +1,38 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean guessNumber = false;
+		Random random = new Random();
+		Scanner scanner= new Scanner(System.in);
+			while ( guessNumber == false) {
+				System.out.println("Pick a number between 1 and 100");
+			}
+				int randomNumber= random.nextInt(100)+1;
+		
+			for(int i =0; i < 5; i++){
+			System.out.println("");
+			
+			int player = scanner.nextInt();
+			
+			if (player == randomNumber) {
+				System.out.println("You Win!");
+			}
+			else if (randomNumber > player) {
+				System.out.println( "Please pick a higher number" );
+			}
+			else { System.out.println("Please pick a lower number");
+
+	}
+	   }
+	}
+
 
 	}
 
-}
+
---
+++ src/com/coderscampus/whileLoop.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+public class whileLoop {
+//	void whileLoop1(){
+	//	int i =0;
+	//	while (i < 100) {
+	//		System.out.println(i);
+		//	i = i +1;
+		}
+	//	System.out.println("Done");
+	}
+
+}
