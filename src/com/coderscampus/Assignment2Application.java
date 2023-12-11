bc92f03afac4c22d4a9391e38018b3d134b99e92
Mondo009 <dossantos989@gmail.com>
2023-12-11 16:54:31 -0500

    Update Assignment2Application.java

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -7,29 +7,28 @@ public class Assignment2Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		boolean guessNumber = false;
 		Random random = new Random();
-		Scanner scanner= new Scanner(System.in);
-			while ( guessNumber == false) {
-				System.out.println("Pick a number between 1 and 100");
-			}
+		try (Scanner scanner = new Scanner(System.in)) {
+			
 				int randomNumber= random.nextInt(100)+1;
-		
-			for(int i =0; i < 5; i++){
-			System.out.println("");
 			
-			int player = scanner.nextInt();
-			
-			if (player == randomNumber) {
-				System.out.println("You Win!");
-			}
-			else if (randomNumber > player) {
-				System.out.println( "Please pick a higher number" );
-			}
-			else { System.out.println("Please pick a lower number");
+				for(int i =0; i < 5; i++){
+				System.out.println("Pick a number between 1 and 100");
+				
+				int guessNumber= scanner.nextInt();
+				
+				
+				if (guessNumber == randomNumber) {
+					System.out.println("You Win!");
+				}
+				else if (randomNumber > guessNumber) {
+					System.out.println( "Please pick a higher number" );
+				}
+				else { System.out.println("Please pick a lower number");
 
-	}
-	   }
+}
+   }
+		}
 	}
 
 
